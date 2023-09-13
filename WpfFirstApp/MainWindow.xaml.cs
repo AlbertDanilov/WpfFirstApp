be58ae9184e52b493cf0cf11d3cff9a2aafcb927
Danilov <albert95danilov@gmail.com>
2023-09-13 18:42:54 +0300

    textBox + button

--- WpfFirstApp/MainWindow.xaml.cs
+++ WpfFirstApp/MainWindow.xaml.cs
@@ -27,7 +27,12 @@ namespace WpfFirstApp
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("WPF from Metanit");
+            string text = texBox1.Text;
+
+            if (!string.IsNullOrEmpty(text.Trim()))
+            {
+                MessageBox.Show(text);
+            }            
         }
     }
 }
