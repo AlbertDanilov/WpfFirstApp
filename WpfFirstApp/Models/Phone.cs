2ea0ca6d3e49b37e8fde17bc63867779b37a38bf
Danilov <albert95danilov@gmail.com>
2023-09-16 14:13:23 +0300

    ValidateValueCallback, CoerceValueCallback

--- WpfFirstApp/Models/Phone.cs
+++ WpfFirstApp/Models/Phone.cs
@@ -15,8 +15,30 @@ namespace WpfFirstApp.Models
         static Phone()
         {
             TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(Phone));
-            PriceProperty = DependencyProperty.Register("Price", typeof(int), typeof(Phone));
+
+            FrameworkPropertyMetadata metadata = new FrameworkPropertyMetadata();
+            metadata.CoerceValueCallback = new CoerceValueCallback(CorrectValue);
+
+            PriceProperty = DependencyProperty.Register("Price", typeof(int), typeof(Phone), metadata,
+                new ValidateValueCallback(ValidateValue));
         }
+
+        private static object CorrectValue(DependencyObject d, object baseValue)
+        {
+            int currentValue = (int)baseValue;
+            if (currentValue > 1000)  // если больше 1000, возвращаем 1000
+                return 1000;
+            return currentValue; // иначе возвращаем текущее значение
+        }
+
+        private static bool ValidateValue(object value)
+        {
+            int currentValue = (int)value;
+            if (currentValue >= 0) // если текущее значение от нуля и выше
+                return true;
+            return false;
+        }
+
         public string Title
         {
             get { return (string)GetValue(TitleProperty); }
