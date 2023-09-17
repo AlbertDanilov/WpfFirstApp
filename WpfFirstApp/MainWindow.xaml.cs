974383aadb479f0fced61583363888ba004a9161
Danilov <albert95danilov@gmail.com>
2023-09-17 11:42:49 +0300

    themes

--- WpfFirstApp/MainWindow.xaml.cs
+++ WpfFirstApp/MainWindow.xaml.cs
@@ -26,13 +26,23 @@ namespace WpfFirstApp
         {
             InitializeComponent();
 
-            
+            List<string> styles = new List<string> { "light", "dark" };
+            styleBox.SelectionChanged += ThemeChange;
+            styleBox.ItemsSource = styles;
+            styleBox.SelectedItem = "dark";
         }
 
-        public void Button_Click(object sender, RoutedEventArgs e)
+        private void ThemeChange(object sender, SelectionChangedEventArgs e)
         {
-            Phone phone = (Phone)this.Resources["nexusPhone"];
-            phone.Company = "LG";
+            string style = styleBox.SelectedItem as string;
+            // определяем путь к файлу ресурсов
+            var uri = new Uri(style + ".xaml", UriKind.Relative);
+            // загружаем словарь ресурсов
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            // очищаем коллекцию ресурсов приложения
+            Application.Current.Resources.Clear();
+            // добавляем загруженный словарь ресурсов
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
         }
     }
 }
