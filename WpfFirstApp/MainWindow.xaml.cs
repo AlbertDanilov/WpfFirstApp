e7d171406ac37c0c56ef6260587df2e7876f7b93
Danilov <albert95danilov@gmail.com>
2023-09-17 10:24:49 +0300

    Window.Resources

--- WpfFirstApp/MainWindow.xaml.cs
+++ WpfFirstApp/MainWindow.xaml.cs
@@ -26,10 +26,5 @@ namespace WpfFirstApp
             InitializeComponent();
         }
 
-        private void Control_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            textBlock1.Text = textBlock1.Text + "sender: " + sender.ToString() + "\n";
-            textBlock1.Text = textBlock1.Text + "source: " + e.Source.ToString() + "\n\n";
-        }
     }
 }
